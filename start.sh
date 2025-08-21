487770d8167e2fbda67c904364e7db42f6475b5f
TBill06 <bill.tushar2@gmail.com>
2025-08-21 04:22:08 -0230

    endgamefinal

--- start.sh
+++ start.sh
@@ -14,6 +14,6 @@ cp "${SECRET_FILE_PATH}" "${TEMP_SESSIONS_PATH}"
 # 2. As root, change the ownership of the COPY to the 'nitter' user.
 chown nitter:nitter "${TEMP_SESSIONS_PATH}"
 
-# 3. Switch to the 'nitter' user and execute the application.
-#    The application will get its file path from the NITTER_SESSION_FILE env var.
-exec su nitter -s /bin/sh -c './nitter'
+# 3. Switch to the 'nitter' user and execute the application,
+#    PASSING THE REDIS CONFIG DIRECTLY ON THE COMMAND LINE.
+exec su nitter -s /bin/sh -c './nitter --conf:redisHost="$REDIS_HOST" --conf:redisPort="$REDIS_PORT"'
