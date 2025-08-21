778af88d165a0b344c553e9060ade946ae8cdb9c
TBill06 <bill.tushar2@gmail.com>
2025-08-21 04:09:45 -0230

    endgame2

--- start.sh
+++ start.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-# This script will run as the root user when the container starts.
+# This script runs as 'root' when the container starts.
 
-# 1. Read the content of the secret file into an environment variable.
-#    'export' makes the variable available to child processes.
-export NITTER_SESSIONS=$(cat /etc/secrets/sessions.jsonl)
+# The path to the secret file mounted by Render.
+SECRET_FILE_PATH="/etc/secrets/sessions.jsonl"
 
-# 2. Use 'su-exec' (a safe way to switch users) to run the final command
-#    as the 'nitter' user.
-#    'su-exec' is like 'sudo' but safer for containers.
-#    Alpine linux (which this image uses) has it built-in.
+# The path to the temporary, writeable copy we will create.
+TEMP_SESSIONS_PATH="/tmp/sessions.jsonl"
+
+# 1. As root, copy the secret file to a writeable location.
+cp "${SECRET_FILE_PATH}" "${TEMP_SESSIONS_PATH}"
+
+# 2. As root, change the ownership of the COPY to the 'nitter' user.
+chown nitter:nitter "${TEMP_SESSIONS_PATH}"
+
+# 3. Switch to the 'nitter' user and execute the application.
+#    The application will get its file path from the NITTER_SESSION_FILE env var.
 exec su nitter -s /bin/sh -c './nitter'
